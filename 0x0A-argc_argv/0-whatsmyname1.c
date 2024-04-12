dc6c401dea40ce27ff315410f30c036ccf623506
franklinegit <oddfrankline@gmail.com>
2024-04-12 11:34:40 +0000

    Alternative implementation of task 0

---
+++ 0x0A-argc_argv/0-whatsmyname1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * 
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * 
+ * Return: 0 when sucessful
+*/
+
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
