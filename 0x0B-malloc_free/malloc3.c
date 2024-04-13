f61f21b7950f6081a76a0d09a33422912d24ff16
franklinegit <oddfrankline@gmail.com>
2024-04-13 20:57:04 +0000

    Adde function to initialize the array

--- 0x0B-malloc_free/malloc3.c
+++ 0x0B-malloc_free/malloc3.c
@@ -21,11 +21,15 @@ typedef struct
 
 int main(void)
 {
+    int no_of_elmts;
+    DynamicArray *arr;
+
     /* Take user input on size of array */
     printf("Enter no of elements to be stored in array: \n");
+    scanf("%d", &no_of_elmts);
 
     /* Initialize the array */
-
+    initArray(arr, no_of_elmts);
 
     /* Take user input to choose what operation to do, and perform operation*/
 
@@ -37,7 +41,20 @@ int main(void)
 }
 
 /* Function for Initializing array */
+void initArray(DynamicArray *arr, int no_of_elmts)
+{
+    arr->array = (DynamicArray *)malloc(no_of_elmts * sizeof(int));
+    if (arr->array == NULL)
+    {
+        fprintf(stderr, "Error. Could not allocate memory: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    arr->size = 0;
+    arr->capacity = no_of_elmts;
 
+    printf("Memory successfully allocated\n");
+    printf("\n");
+}
 
 /* Function to add element */
 
