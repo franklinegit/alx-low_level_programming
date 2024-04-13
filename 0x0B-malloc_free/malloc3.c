945087ae8f28f35e2e44888af88b7bb4b9aba914
franklinegit <oddfrankline@gmail.com>
2024-04-13 19:45:45 +0000

    Framework for the program

---
+++ 0x0B-malloc_free/malloc3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <string.h>
+
+/* Structure for array */
+typedef struct
+{
+    int *array;
+    int size;
+    int capacity;
+}DynamicArray;
+
+
+/**
+ * main - Entry function for initializing, element addition, printing, quiting, and freeing
+ * 
+ * Return: EXIT_SUCCESS, EXIT_FAILURE
+*/
+
+int main(void)
+{
+    /* Take user input on size of array */
+
+
+    /* Initialize the array */
+
+
+    /* Take user input to choose what operation to do, and perform operation*/
+
+
+    /* Free allocated memory */
+
+
+    return (EXIT_SUCCESS);
+}
+
+/* Function for Initializing array */
+
+
+/* Function to add element */
+
+
+/* Function to print array elemts */
+
+
+/* Function to free memory */
