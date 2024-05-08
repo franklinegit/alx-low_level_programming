d19e3d6c33dce29628c85232f72062471edee28c
franklinegit <oddfrankline@gmail.com>
2024-05-08 21:10:48 +0000

    BinarySearch function for the binary search algorithm program

--- 0x08-recursion/binary_search.c
+++ 0x08-recursion/binary_search.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+int binarySearch(int arr[], int lowest_idx, int highest_idx, int target)
+{
+    if (lowest_idx >= highest_idx)
+    {
+        if (lowest_idx == highest_idx && arr[lowest_idx] == target)
+        {
+            return (lowest_idx);
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+
+    int mid_idx = lowest_idx + (highest_idx - lowest_idx) / 2;
+
+    if (arr[mid_idx] == target)
+        return (mid_idx);
+
+    else if (arr[mid_idx] > target)
+        return (binarySearch(arr, lowest_idx, mid_idx - 1, target));
+
+    else
+        return (binarySearch(arr, mid_idx + 1, highest_idx, target));
+}
+
 
 int main(void)
 {
